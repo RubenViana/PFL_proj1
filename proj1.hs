7700e2b9e2738f6465fc0af66402fa6f77dc34b6
Ruben Viana <up202005108@up.pt>
2022-10-18 09:18:53 +0100

    new struct to represent polys

--- proj1.hs
+++ proj1.hs
@@ -20,7 +20,7 @@ sortPoly (x:y:xs)
 completeSortPoly xs = iterate sortPoly xs !! length xs
 
 menu = do
-        putStr "[1] Normalize polynomial\n[2] Add polynomials\n[3] Multiply polynomials\n[4] Derivate polynomial\n\n"
+        putStr "[1] Normalize polynomial\n[2] Add polynomials\n[3] Multiply polynomials\n[4] Derive polynomial\n\n"
         putStr "Option: "
         str <- getLine
         putStr "\n"
@@ -38,6 +38,7 @@ menu = do
 
 normalizePoly p = printPoly (norm (completeSortPoly p))
 addPoly p1 p2 = normalizePoly (addp (norm (completeSortPoly p1)) (norm (completeSortPoly p2)))
+derivePoly p = normalizePoly (derive (norm (completeSortPoly p)))
 
 norm :: [[Int]] -> [[Int]]
 norm [] = []
@@ -57,4 +58,77 @@ addp :: [[Int]] -> [[Int]] -> [[Int]]
 addp p [] = p
 addp p1 p2 = addp (p1 ++ [head p2]) (tail p2)
 
+
+--derive a*x^i = a*i*x^(i-1)
+
+derive :: [[Int]] -> [[Int]]
+derive [] = []
+derive (x:xs) = [x !! 0, (x !! 1) - 1, (x !! 2) * (x !! 1)] : derive (xs)
+
+
+{--data Term = Term {coe :: Float, var :: Char, degree :: Int} deriving (Show)
+
+getCoe :: Term -> Float
+getCoe (Term coe _ _) = coe
+
+getVar :: Term -> Char
+getVar (Term _ var _) = var
+
+getDeg :: Term -> Int
+getDeg (Term _ _ deg) = deg
+
+createTerm :: Float -> Char -> Int -> Term
+createTerm c v d = Term c v d
+
+x = createTerm 3 'x' 2
+y = createTerm 4 'y' 1
+z = createTerm 2 'y' 1
+
+polyList = [x,y,z] --}
+
+data Poly = Poly {var :: String, coes :: [Float]} deriving (Show, Ord, Eq)
+
+createPoly :: Float -> String -> Int -> Poly
+createPoly c v d = Poly v (insertAt c d [])
+
+addToPolyList :: Float -> String -> Int -> [Poly] -> [Poly]
+addToPolyList c v d [] = [createPoly c v d] 
+--addToPolyList c v d pl 
+--                    |v in pl.getAllVars = insertAt c (d - 1) (getCoes plv) --pseudocode
+--                    |otherwise = pl ++ [createPoly c v d]
+
+getCoes :: Poly -> [Float]
+getCoes (Poly _ coes) = coes
+
+getVar :: Poly -> String
+getVar (Poly var _) = var
+
+getAllVars :: [Poly] -> [String]
+getAllVars [] = []
+--getAllVars pl = filter (\getVar _ -> var) pl
+
+stringToListPolys :: String -> [Poly]
+stringToListPolys "" = []
+--stringToListPolys -- '+/-/ ' -> int -> '*' -> char -> '^' -> int  <<== maybe state machine! or create a func for each type o value...
+
+showPoly pl = concat[concat[" + " ++ show c ++ "*" ++ getVar p ++ "^" ++ show (length (getCoes p)) | c <- reverse (getCoes p), c > 0] | p <- sort pl]
+
+insertAt :: Float -> Int -> [Float] -> [Float]
+insertAt ne i [] = (take i (repeat 0)) ++ [ne] 
+insertAt newElement i (a:as)
+  | length(as)+1 < i = insertAt newElement i (zp (a:as) (take i (repeat 0)))
+  | i <= 0 = newElement:a:as
+  | otherwise = a : insertAt newElement (i - 1) as
+
+zp :: [Float] -> [Float] -> [Float]
+zp a b = if (length a >= length b)
+                then zipWith (+) a (b ++ repeat 0)
+                else zp b a
+
+a = [0,1,2,3]
+b = [0,0,0,0,0,0,0,0]
+
+-- example: 0*x^2 + 2*y + 5*z + y + 7*y^2
+example :: [Poly]
+example = [createPoly 0 "x" 2, createPoly 2 "y" 1, createPoly 5 "z" 1, createPoly 1 "y" 1, createPoly 7 "y" 2]
 
