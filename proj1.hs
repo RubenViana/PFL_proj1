b6d4f8ed899edfa4118246700cea07f47e7c4b4c
Ruben Viana <up202005108@up.pt>
2022-10-20 21:39:19 +0100

    almost done, TODO multiplication

--- proj1.hs
+++ proj1.hs
@@ -1,102 +1,29 @@
 import Data.List
 import Data.Char
-
---poly type -> [var, degree, coe] = [ord('x'), 2, 3] = "3*x^2"
-
---example poly
-
---p1 = [[ord 'x', 2, 0], [ord 'y', 1, 2], [ord 'z', 1, 5], [ord 'y', 1, 1], [ord 'y', 2, 7]]
---p2 = [[ord 'z', 1, 10], [ord 'x', 2, 3]]
-
-{--sortPoly :: [[Int]] -> [[Int]]
-sortPoly [] = []
-sortPoly [x] = [x]
-sortPoly (x:y:xs)
-                |(x !! 0 < y !! 0) = x : sortPoly (y:xs)
-                |(x !! 0 > y !! 0) = y : sortPoly (x : xs)
-                |(x !! 0 == y !! 0) && (x !! 1 >= y !! 1) = x : sortPoly (y : xs)
-                |otherwise = y : sortPoly (x : xs)
-
-completeSortPoly xs = iterate sortPoly xs !! length xs
-
-menu = do
-        putStr "[1] Normalize polynomial\n[2] Add polynomials\n[3] Multiply polynomials\n[4] Derive polynomial\n\n"
-        putStr "Option: "
-        str <- getLine
-        putStr "\n"
-        let opt = read str
-        if (opt == 1) then
-            putStr "Func format : normalizePoly [[ord 'var', degree, coe], ...]\n"
-        else if (opt == 2) then
-            putStr "not implemented\n"
-        else if (opt == 3) then
-            putStr "not implemented\n"
-        else if (opt == 4) then
-            putStr "not implemented\n"
-        else
-            putStr "exit\n"
-
-normalizePoly p = printPoly (norm (completeSortPoly p))
-addPoly p1 p2 = normalizePoly (addp (norm (completeSortPoly p1)) (norm (completeSortPoly p2)))
-derivePoly p = normalizePoly (derive (norm (completeSortPoly p)))
-
-norm :: [[Int]] -> [[Int]]
-norm [] = []
-norm [xs] = [xs]
-norm (x:xs) 
-            |((x !! 0) == (y !! 0)) && ((x !! 1) == (y !! 1)) = [x !! 0, x !! 1, (x !! 2) + (y !! 2)] : norm (tail xs)
-            |x !! 2 == 0 = norm xs
-            |otherwise = x : norm xs
-            where y = head xs
-
-printPoly :: [[Int]] -> String
-printPoly [] = ""
-printPoly p = tail(concat[op ++ show c ++ "*" ++ [(chr (v))] ++ deg| [v, d, c] <- sortPoly p, let op = if (c >= 0) then " +" else " ", let deg = if (d == 1) then "" else ("^" ++ show d)])
+import Data.List.Split
 
 
-addp :: [[Int]] -> [[Int]] -> [[Int]]
-addp p [] = p
-addp p1 p2 = addp (p1 ++ [head p2]) (tail p2)
-
-
---derive a*x^i = a*i*x^(i-1)
-
-derive :: [[Int]] -> [[Int]]
-derive [] = []
-derive (x:xs) = [x !! 0, (x !! 1) - 1, (x !! 2) * (x !! 1)] : derive (xs)
-
-
-data Term = Term {coe :: Float, var :: Char, degree :: Int} deriving (Show)
-
-getCoe :: Term -> Float
-getCoe (Term coe _ _) = coe
-
-getVar :: Term -> Char
-getVar (Term _ var _) = var
-
-getDeg :: Term -> Int
-getDeg (Term _ _ deg) = deg
-
-createTerm :: Float -> Char -> Int -> Term
-createTerm c v d = Term c v d
-
-x = createTerm 3 'x' 2
-y = createTerm 4 'y' 1
-z = createTerm 2 'y' 1
-
-polyList = [x,y,z] --}
+{-- 
+    POLYNOMIAL DATA FUNCTONS
+--}
 
 data Poly = Poly {var :: String, coes :: [Float]} deriving (Show, Ord, Eq)
 
 createPoly :: Float -> String -> Int -> Poly
-createPoly c v d = Poly v (insertAt c d [])
+createPoly c v d = Poly var (insertAt c d []) where var = if (d == 0) then "zzzz" else v
 
-addToPolyList :: Float -> String -> Int -> [Poly] -> [Poly]
-addToPolyList c v d [] = [createPoly c var d] where var = if (d == 0) then "zzzz" else v
-addToPolyList c v d pl 
+
+addTermToPolyList :: Float -> String -> Int -> [Poly] -> [Poly]
+addTermToPolyList c v d pl 
                     |(isMember var (getAllVars pl)) = Poly var (updateCoes c d (getCoes (pl !! (head (elemIndices var (getAllVars pl)))))) : [p | p <- pl, getVar p /= var]
                     |otherwise = pl ++ [createPoly c var d]
                     where var = if (d == 0) then "zzzz" else v
+
+
+addPolyToPolyList :: Poly -> [Poly] -> [Poly]
+addPolyToPolyList p pl
+                    |(isMember (getVar p) (getAllVars pl)) = Poly (getVar p) (zp (getCoes p) (getCoes (pl !! (head (elemIndices (getVar p) (getAllVars pl)))))) : [p1 | p1 <- pl, getVar p1 /= getVar p]
+                    |otherwise = pl ++ [p]
 
 getCoes :: Poly -> [Float]
 getCoes (Poly _ coes) = coes
@@ -111,20 +38,50 @@ getAllVars pl = map getVar pl
 updateCoes :: Float -> Int -> [Float] -> [Float]
 updateCoes n i c = zp (insertAt n i (take i (repeat 0))) c
 
-stringToListPolys :: String -> [Poly]
-stringToListPolys "" = []
---stringToListPolys -- '+/-/ ' -> int -> '*' -> char -> '^' -> int  <<== maybe state machine! or create a func for each type o value...
+stringListToPolyList :: [String] -> [Poly]
+stringListToPolyList [] = []
+stringListToPolyList (x:xs) = [toPoly x] ++ (stringListToPolyList xs)
 
-addPolys :: [Poly] -> [Poly] -> [Poly]
-addPolys [pl] _ = [pl]
-addPolys pl1 [] = pl1
-addPolys pl1 (p2:pl2) = addPolys (concat[addToPolyList c (getVar p2) d pl1 |  c <- getCoes p2, c /= 0, let d = head (elemIndices c (getCoes p2))]) pl2
+stringToPolyList :: String -> [Poly]
+stringToPolyList s = stringListToPolyList (splitString s)
+
+takeCoe :: [Char] -> [Char]
+takeCoe [x] = [x]
+takeCoe x = takeWhile (/='*') x
+
+takeVar :: [Char] -> [Char]
+takeVar [x] = [x]
+takeVar x = takeWhile (/='^') (dropWhile (\i -> not (isAlpha i)) x)
+
+takeDeg :: [Char] -> [Char]
+takeDeg x = let l = (dropWhile (/='^') x) in if ((length (l) > 0) && ((length x) /= 1)) then tail l else "1"
+
+toPoly :: String -> Poly
+toPoly [] = createPoly 0 " " 0 
+toPoly (x:xs)
+  | ((x == '+') && not('*' `elem` xs) && isDigit(head xs)) = createPoly (read(xs)::Float) " " 0
+  | ((x == '-') && not('*' `elem` xs) && isDigit(head xs)) = createPoly (negate (read(xs)::Float)) " " 0
+  | (not('*' `elem` (x:xs)) && isDigit(x)) = createPoly (read(x:xs)::Float) " " 0
+  | (x == '+' && isDigit(head xs)) = createPoly (read (takeCoe xs)::Float) (takeVar xs) (read (takeDeg xs)::Int)
+  | (x == '-' && isDigit(head xs)) = createPoly (negate (read (takeCoe xs)::Float)) (takeVar xs) (read (takeDeg xs)::Int)
+  | (isDigit(x)) = createPoly (read (takeCoe (x:xs))::Float) (takeVar (x:xs)) (read (takeDeg (x:xs))::Int)
+  | (x == '+') = createPoly 1 (takeVar xs) (read (takeDeg xs)::Int)
+  | (x == '-') = createPoly (-1) (takeVar xs) (read (takeDeg xs)::Int)
+  | otherwise = createPoly 1 (takeVar (x:xs)) (read (takeDeg (x:xs))::Int) 
+
+normRec :: [Poly] -> [Poly] -> [Poly]
+normRec pl [] = pl
+normRec pl (x:xs) = normRec (addPolyToPolyList x pl) xs
 
 derivePoly :: [Poly] -> [Poly]
 derivePoly [] = []
-derivePoly (p:pl) = (concat[addToPolyList c (getVar p) (d) [] | c <- derive (getCoes p), c/=0, let d = head (elemIndices c (derive (getCoes p)))]) ++ derivePoly pl
+derivePoly (p:pl) = (concat[addTermToPolyList c (getVar p) (d) [] | c <- derive (getCoes p), c/=0, let d = head (elemIndices c (derive (getCoes p)))]) ++ derivePoly pl
 
 showPoly pl = concat[concat[sig ++ show (abs c) ++ var ++ exp | c <- reverse (getCoes p), c /= 0, let sig = if (c >= 0) then " + " else " - ", let d = head (elemIndices c (getCoes p)), let var = if (d == 0) then "" else ("*" ++ getVar p), let exp = if (d <= 1) then "" else ("^" ++ show d)] | p <- sort pl]
+
+{--
+     AUX FUNCTIONS
+--}
 
 insertAt :: Float -> Int -> [Float] -> [Float]
 insertAt ne i [] = (take i (repeat 0)) ++ [ne] 
@@ -147,13 +104,75 @@ isMember n (x:xs)
     | n == x = True
     | otherwise = isMember n xs
 
+splitString :: String -> [String]
+splitString s = split (startsWithOneOf ['+','-']) (dropSpaces s)
 
--- example: 0*x^2 + 2*y + 5*z + y + 7*y^2
-example :: [Poly]
-example = [createPoly 0 "x" 2, createPoly 2 "y" 1, createPoly 5 "z" 1, createPoly 1 "y" 1, createPoly 7 "y" 2]
-
-
-pp1 = addToPolyList (-7) "y" 2 (addToPolyList 1 "y" 0 (addToPolyList 5 "z" 1 (addToPolyList 2 "y" 1 (addToPolyList 0 "x" 2 []))))
-pp2 = addToPolyList 10 "" 0 (addToPolyList 5 "x" 2 (addToPolyList (3) "" 0 []))
+dropSpaces :: String -> String
+dropSpaces s = filter (\x -> (x /= ' ')) s
 
 
+
+{-- 
+    MAIN FUNCTIONS
+--}
+
+normalizePolynomial :: [Poly] -> [Poly]
+normalizePolynomial [] = []
+normalizePolynomial pl = normRec [] pl
+
+addPolynomials :: [Poly] -> [Poly] -> [Poly]
+addPolynomials [] pl = pl
+addPolynomials pl [] = pl
+addPolynomials pl1 pl2 = normalizePolynomial (pl1 ++ pl2)
+
+multiplyPolynomials :: [Poly] -> [Poly] -> [Poly]
+multiplyPolynomials [] pl = pl
+multiplyPolynomials pl [] = pl
+--multiplyPolynomials pl1 pl2 = ...             TODO
+
+derivePolynomial :: [Poly] -> [Poly]
+derivePolynomial [] = []
+derivePolynomial pl = normalizePolynomial (derivePoly pl)
+
+
+
+{-- 
+    MAIN MENU
+--}
+
+main = do
+        putStr "[1] Normalize polynomial\n[2] Add polynomials\n[3] Multiply polynomials\n[4] Derive polynomial\n[0] Exit\n\n"
+        putStr "Option: "
+        str <- getLine
+        putStr "\n"
+        let opt = read str
+        if (opt == 1) then
+            do
+                putStr "Enter Polynomial: "
+                pol <- getLine
+                putStrLn (showPoly (normalizePolynomial (stringToPolyList pol)))
+        else if (opt == 2) then
+            do
+                putStr "Enter Polynomial 1: "
+                pol1 <- getLine
+                putStr "Enter Polynomial 2: "
+                pol2 <- getLine
+                putStrLn (showPoly (addPolynomials (stringToPolyList pol1) (stringToPolyList pol2)))
+        else if (opt == 3) then
+            do
+                putStr "Enter Polynomial 1: "
+                pol1 <- getLine
+                putStr "Enter Polynomial 2: "
+                pol2 <- getLine
+                putStrLn "(showPoly (multiplyPolynomials (stringToPolyList pol1) (stringToPolyList pol2)))" -- NOT IMPLEMENTED
+        else if (opt == 4) then
+            do
+                putStr "Enter Polynomial: "
+                pol <- getLine
+                putStrLn (showPoly (derivePolynomial (stringToPolyList pol)))
+        else if (opt == 0) then
+            putStrLn "exit"
+        else 
+            main
+
+-- example: 0*x^2 + 2*y -5*z + y + 7*y^2
